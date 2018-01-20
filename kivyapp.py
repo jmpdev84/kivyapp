0f601c8b387f7c9f703c677dd82cdb542667401d
Jason Potter <jmpdev84@gmail.com>
2018-01-20 15:03:54 -0500

    added image files and support for reading from ini file

--- kivyapp.py
+++ kivyapp.py
@@ -13,10 +13,20 @@ from kivy.uix.button import Button
 from kivy.uix.button import Label
 from kivy.uix.widget import Widget
 
+import kivyappconfig
+import os.path
+
+#Write default ini file if it doesn't exist
+if not os.path.isfile('kivyapp.ini'):
+    kivyappconfig.setup()
 
 class PasswordScreen(Screen):
+    appPassword = StringProperty()
     def password_check(self, password):
-        if password == 'password':
+        config = kivyappconfig.get_config_parser()
+        config.read('kivyapp.ini')
+        appPassword = config.get('default', 'password', fallback='123')
+        if password == appPassword:
             print('success')
             self.manager.current = 'imageScrn'
         else:
@@ -25,20 +35,48 @@ class PasswordScreen(Screen):
 class ImageScreen(Screen):
     imageGuess = StringProperty('')
 
+    def reset_buttons(self):
+        for button in range(1, 10):
+            self.ids[str(button)].opacity = 1
+
     def image_check(self, instance):
+        #print('id is ' + instance.id)
+        #instance.parent.ids.lid.text = self.get_id(instance)
+        #print(instance.parent.ids.lid.text)
+        instance.opacity = 0.5
+        #l = Label(id='lbl_1', text='1', font_size='30')
+        #instance.add_widget(l)
+        #instance.text = "1"
         self.imageGuess += instance.text
         if len(self.imageGuess) == 3:
             print('Guess was: ' + self.imageGuess)
             if self.imageGuess == '123':
                 self.imageGuess = ''
+                #Reset buttons
+                for button in range(1, 10):
+                    self.ids[str(button)].opacity = 1
                 self.manager.current = 'successScrn'
             else:
                 self.imageGuess = ''
+                #Reset Buttons
+                for button in range(1, 10):
+                    self.ids[str(button)].opacity = 1
+
+    def get_id(self,  instance):
+        for id, widget in instance.parent.ids.items():
+            if widget.__self__ == instance:
+                return str(id)
 
 class SuccessScreen(Screen):
     def reset(self):
         passwordField.text = ''
         self.manager.current = 'passwordScrn'
+
+        #reset image buttons
+        for button in range(1, 9):
+            print(button)
+            self.ids[str(button)].opacity = 1
+
 
 class ScreenManagement(ScreenManager):
     pass
@@ -49,5 +87,7 @@ class MyKivyApp(App):
 
     def build(self):
         return presentation
+
+
 
 MyKivyApp().run()
---
+++ kivyappconfig.py
@@ -0,0 +1,24 @@
+import configparser
+
+def setup():
+    config = configparser.ConfigParser()
+    config['default'] = {}
+    config['default']['password'] = 'password123'
+
+    config['images'] = {}
+    config['images']['image1'] = './circle.png'
+    config['images']['image2'] = './cross.png'
+    config['images']['image3'] = './dots.png'
+    config['images']['image4'] = './hexagon.png'
+    config['images']['image5'] = './hexagons.png'
+    config['images']['image6'] = './rhombus.png'
+    config['images']['image7'] = './square.png'
+    config['images']['image8'] = './triangle.png'
+    config['images']['image9'] = './triforce.png'
+    config['images']['randomize'] = 'True'
+    
+    with open('kivyapp.ini', 'w') as configfile:
+        config.write(configfile)
+
+def get_config_parser():
+    return configparser.ConfigParser()
